567f5e0335feecb976c07e7f84e99fc0ebec4c66
Fisto Riza Satianto <fistoriza@me.com>
2017-02-26 08:30:47 +0000

    enrollments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226064453) do
+ActiveRecord::Schema.define(version: 20170226075721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170226064453) do
     t.datetime "updated_at",  null: false
     t.string   "image"
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
